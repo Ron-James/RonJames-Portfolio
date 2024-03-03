8651ad8165a3fa0d7d33f1b0a8633fabe79dfd41
BizarRonJames <jajotheron@gmail.com>
2024-03-03 13:10:24 +0200

    updates

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -21,27 +21,46 @@ export default class Home extends Component {
             software projects I have been involved in."
         />
 
-<TextSection
+        <TextSection
           Image="road.jpg"
           Sub=""
           Desc="Good Evening! My name is James 'Ron James' Theron. I'm a
             qualified software engineer and game designer by trade. I'm qualified
-            with a BSc in Engineering Sciences and have my honours in Game Design and Interactive Development from the
-            University of the Witwatersrand. I'm a hardworking and confident programmer navigating my way throught the game development industry."
+            with a BSc in Engineering Sciences, have my honours in Game Design and Interactive Development from the
+            University of the Witwatersrand and have professional experience working as a programmer in game development. I'm a hardworking and confident programmer navigating my way throught the game development industry."
           Title="About Me"
           TextColor="white"
         />
+
+        <TextSection
+          Image="work.png"
+          Sub=""
+          Desc="I am currently working full time as a Unity Software Developer at a company called BizAR Reality. I work predominantly within  the Virtual Reality space develping for the Oculus Quest.
+          Since March 2023, I have worked on many different VR Unity projects and have learned a lot about the VR space. 
+          Many of these projects have also involved multiplayer capabilities and I have become familiar with Unity networking libraries like Mirror and Normcore. Working here at BizAR reality has forced me to become a more adaptable programmer with fast output. I feel I have come along way since I started working here and I feel I have a lot to offer in terms of strong problem solving and reliable output."
+          Title="Work Experience"
+          TextColor="white"
+        />
+
         <TextSection
           Image="dragon.jpg"
           Sub=""
-          Desc="I specialise in C++/C# development and have developed several
+          Desc="I specialise in C# development and have developed several
           computer games in the Unity game engine. I have worked on both solo
           and team projects in a personal and professional setting. When it comes to developing digital games, my
           skills are best utilised in: general scripting, mechanic design,
           physics engine design, developer tool creation, Virtual Reality development and multiplayer architecture development.
-          I have been using Unity to develope games since 2019
+          I have been using Unity to develop games since 2019
           and have become well aquainted with the software."
-          Title="Game Designer and Developer"
+          Title="Unity Development"
+          TextColor="whitesmoke"
+        />
+
+        <TextSection
+          Image="night.png"
+          Sub=""
+          Desc="Alongside my professional work in the Unity Game Engine, I have also started learning to develop games in the Unreal Engine. I have found a lot of joy in working with the engine and find it has a lot of advantages to offer in game development. This is still currently a work in progress but I feel my skills in Unity have given me a good foundation to start learning Unreal. I have been working on a few small projects and tutorials in Unreal and have been learning the basics of the engine. I am confident that I will be able to develop games in Unreal in the near future."
+          Title="Unreal Engine Development"
           TextColor="whitesmoke"
         />
 
