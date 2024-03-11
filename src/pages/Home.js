61eeb20903bc0bc353c2084cde0fec535adea5d4
BizarRonJames <jajotheron@gmail.com>
2024-03-11 10:41:50 +0200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -21,10 +21,8 @@ export default class Home extends Component {
         <TextSection
           Image="road.jpg"
           Sub=""
-          Desc="Good Evening! My name is James 'Ron James' Theron. I'm a
-            qualified software engineer and game designer by trade. I'm qualified
-            with a BSc in Engineering Sciences, have my honours in Game Design and Interactive Development from the
-            University of the Witwatersrand and have professional experience working as a programmer in game development. I'm a hardworking and confident programmer navigating my way throught the game development industry."
+          Desc="
+          Good evening! I'm James 'Ron James' Theron, a software engineer and game designer with a BSc in Engineering Sciences and honors in Game Design and Interactive Development from the University of the Witwatersrand. With professional experience in game development, I navigate the industry with confidence and a strong work ethic. I'm passionate about innovative game design and committed to pushing the boundaries of interactive development."
           Title="About Me"
           TextColor="white"
         />
@@ -32,9 +30,7 @@ export default class Home extends Component {
         <TextSection
           Image="work.png"
           Sub=""
-          Desc="I am currently working full time as a Unity Software Developer at a company called BizAR Reality. I work predominantly within  the Virtual Reality space develping for the Oculus Quest.
-          Since March 2023, I have worked on many different VR Unity projects and have learned a lot about the VR space. 
-          Many of these projects have also involved multiplayer capabilities and I have become familiar with Unity networking libraries like Mirror and Normcore. Working here at BizAR reality has forced me to become a more adaptable programmer with fast output. I feel I have come along way since I started working here and I feel I have a lot to offer in terms of strong problem solving and reliable output."
+          Desc="I am currently employed full-time as a Unity Software Developer at BizAR Reality, specializing in Virtual Reality development for the Oculus Quest. Since March 2023, I have contributed to various VR projects, enriching my understanding of the VR landscape. Many of these projects have incorporated multiplayer features, allowing me to become proficient in Unity networking libraries like Mirror and Normcore. My time at BizAR Reality has honed my adaptability as a programmer, enhancing my ability to deliver high-quality work efficiently. I am proud of the progress I have made and confident in my problem-solving abilities and consistent performance."
           Title="Work Experience"
           TextColor="white"
         />
@@ -42,13 +38,7 @@ export default class Home extends Component {
         <TextSection
           Image="dragon.jpg"
           Sub=""
-          Desc="I specialise in C# development and have developed several
-          computer games in the Unity game engine. I have worked on both solo
-          and team projects in a personal and professional setting. When it comes to developing digital games, my
-          skills are best utilised in: general scripting, mechanic design,
-          physics engine design, developer tool creation, Virtual Reality development and multiplayer architecture development.
-          I have been using Unity to develop games since 2019
-          and have become well aquainted with the software."
+          Desc="I specialize in C# development and have created numerous computer games using the Unity game engine. My experience spans solo and team projects in both personal and professional environments. My expertise lies in scripting architecture, mechanic design, physics engine design, developer tool creation, Virtual Reality development, and multiplayer architecture. Since 2019, I have been deeply engaged with Unity, gaining a strong familiarity with the software."
           Title="Unity Development"
           TextColor="whitesmoke"
         />
@@ -79,13 +69,7 @@ export default class Home extends Component {
         <TextSection
           Image="stars.jpg"
           Sub=""
-          Desc="My skills outside the technical world of programming include project
-          management and working with other people. Having worked on several
-          small team projects and taking the role of project manager in a few
-          of them. I feel I can be a valuable team player on a variety of
-          different projects in the future. I am particuarly passionate about
-          making games and am perfectly capable on putting my heart and soul
-          into a project."
+          Desc="I possess strong skills in game design and collaboration. Through involvement in numerous small team projects and serving as project manager in several, I have honed my ability to work effectively with others. I believe I can contribute significantly as a team player across a range of future projects. My passion for game development drives me to dedicate myself fully to each endeavor."
           Title="Being a Team Player"
           TextColor="whitesmoke"
         />
